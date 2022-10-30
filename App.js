54866c3c16b35db0f381acac8d12f34c2ca78b97
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-10-30 03:08:58 -0700

    event

---
+++ App.js
@@ -0,0 +1,96 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==================================================================
+
+// let name = document.getElementById("name")
+// let password = document.getElementById("password")
+
+
+
+let a = document.getElementById("name")
+let b = document.getElementById("password")
+let button = document.getElementById("btn")
+
+
+button.addEventListener("click" , ()=>{
+    document.write(`<h2>Your name is ${a.value} <br><br> Your password is ${b.value} </h2>`);
+})
+
+
+
+
+
+
+
+
+
+
+
